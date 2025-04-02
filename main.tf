09fccb528a8daba0d29118e6c97a3340277f0d68
ckekula <chamukeku@gmail.com>
2025-04-02 16:42:57 +0530

    add directory creation before file copy

--- main.tf
+++ main.tf
@@ -7,7 +7,19 @@ variable "dockerhub_username" {
   type        = string
 }
 
-# Reference existing security group by name or ID
+variable "production_ssh_key" {
+  description = "SSH key for production server"
+  type        = string
+  default     = ""  # Will be passed from GitHub secrets
+}
+
+variable "github_sha" {
+  description = "GitHub SHA from the triggering commit"
+  type        = string
+  default     = "latest"
+}
+
+# Reference existing security group by name
 data "aws_security_group" "jenkins_sg" {
   name = "jenkins-sg"  # should be created manually
 }
@@ -20,6 +32,34 @@ resource "aws_instance" "jenkins" {
 
   tags = {
     Name = "temporary-jenkins"
+  }
+
+  # First, create necessary directories on the EC2 instance
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/ubuntu/ansible/files",
+      "mkdir -p /home/ubuntu/jenkins_config"
+    ]
+    
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/jenkins-key.pem")
+      host        = self.public_ip
+    }
+  }
+
+  # Copy Ansible playbook file
+  provisioner "file" {
+    source      = "ansible/jenkins-setup.yml"
+    destination = "/home/ubuntu/ansible/jenkins-setup.yml"
+    
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/jenkins-key.pem")
+      host        = self.public_ip
+    }
   }
 
   # Copy Ansible files to the instance
@@ -40,18 +80,26 @@ resource "aws_instance" "jenkins" {
     inline = [
       "sudo apt update",
       "sudo apt install -y ansible",
-      "sudo mkdir -p /home/ubuntu/jenkins_config",
-      "sudo mkdir -p /home/ubuntu/ansible",
-      "ls -la /home/ubuntu",
+
       # Configure SSH to not require host verification for localhost
       "mkdir -p ~/.ssh",
       "echo 'StrictHostKeyChecking no' > ~/.ssh/config",
       "chmod 600 ~/.ssh/config",
+
       # Export environment variables
       "export DOCKERHUB_USERNAME=${var.dockerhub_username}",
+      "export PRODUCTION_SSH_KEY='${var.production_ssh_key}'",
+      "export GITHUB_SHA=${var.github_sha}",
+
+      # Debug: List files to verify they were copied correctly
+      "echo 'Listing ansible directory:'",
+      "ls -la /home/ubuntu/ansible",
+      "echo 'Listing files directory:'",
+      "ls -la /home/ubuntu/ansible/files",
+      
       # Run ansible playbook
       "cd /home/ubuntu/ansible",
-      "ansible-playbook -i 'localhost,' -c local jenkins-setup.yml",
+      "ansible-playbook -i 'localhost,' -c local jenkins-setup.yml -v"
     ]
 
     connection {
