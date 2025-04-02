03f2e74e1404ddbf310a1e98d3dc9f3695b13230
ckekula <chamukeku@gmail.com>
2025-04-02 12:24:10 +0530

    remove security group creation

--- main.tf
+++ main.tf
@@ -7,31 +7,31 @@ variable "dockerhub_username" {
   type        = string
 }
 
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-  description = "Allow SSH and Jenkins port"
+# resource "aws_security_group" "jenkins_sg" {
+#   name        = "jenkins-sg"
+#   description = "Allow SSH and Jenkins port"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # restrict in production
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # restrict in production
+#   }
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # restrict in production
-  }
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # restrict in production
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # restrict in production
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"] # restrict in production
+#   }
+# }
 
 resource "aws_instance" "jenkins" {
   ami                    = "ami-0e35ddab05955cf57"  # Ubuntu Server 24.04 LTS
