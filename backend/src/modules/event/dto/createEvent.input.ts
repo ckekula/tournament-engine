042da26ecd8cc82f2e431e7452a65edbbf9ef718
ckekula <chamukeku@gmail.com>
2025-04-16 23:25:39 +0530

    bugfix

--- backend/src/modules/event/dto/createEvent.input.ts
+++ backend/src/modules/event/dto/createEvent.input.ts
@@ -4,7 +4,6 @@ import { IsNotEmpty, IsString, MaxLength, MinLength, IsNumber } from 'class-vali
 @InputType()
 export class CreateEventInput {
   @Field()
-  @IsNotEmpty({ message: 'Name is required' })
   @IsString({ message: 'Name must be a string' })
   @MinLength(2, { message: 'Name must be at least 2 characters' })
   @MaxLength(100, { message: 'Name must be at most 100 characters' })
